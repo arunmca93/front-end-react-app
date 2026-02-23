af957be5e63921b350b3ce98cd03da148cc93a37
Arunkumar G <arunmca93@gmail.com>
2026-02-23 23:11:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 echo "Building static HTML site..."
-sudo su
-dnf install -y nodejs22
+sudo dnf install -y nodejs22
 npm install
 npm run build
 echo "Build complete. Output in /build"
