32a25105cd6767bfd2c564a69e0f934304940e28
Arunkumar G <arunmca93@gmail.com>
2026-02-23 23:13:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 echo "Building static HTML site..."
-sudo dnf install -y nodejs22
+## Install once in Jenkins server
+# sudo dnf install -y nodejs22 
 npm install
 npm run build
 echo "Build complete. Output in /build"
